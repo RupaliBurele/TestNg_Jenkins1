f77f23075648d3ddd5dadc1cfd3f5d0c74a62439
Rupali <Rupali@192.168.1.11>
2020-09-09 18:44:17 +0530

    new login 2 commited

--- TestNG_Practice_jenkins/src/test/java/com/jenkins/practice/LoginTest.java
+++ TestNG_Practice_jenkins/src/test/java/com/jenkins/practice/LoginTest.java
@@ -15,15 +15,15 @@ public class LoginTest {
 	public void setUp() {
 		System.setProperty("webdriver.chrome.driver","D:\\core java classes\\Softwares\\Selenium\\chrome\\chromedriver.exe");
 
-		 driver=new ChromeDriver();
+		driver=new ChromeDriver();
 
 		driver.get("http://demowebshop.tricentis.com/");
 
 		driver.manage().window().maximize();
-		
+
 		driver.manage().timeouts().pageLoadTimeout(30,TimeUnit.SECONDS);
 	}
-	
+
 	@Test
 	public void login1() {
 		driver.findElement(By.xpath("/html/body/div[4]/div[1]/div[1]/div[2]/div[1]/ul/li[2]/a")).click();
@@ -32,9 +32,21 @@ public class LoginTest {
 		driver.findElement(By.xpath("//*[@id=\"RememberMe\"]")).click();
 		driver.findElement(By.xpath("/html/body/div[4]/div[1]/div[4]/div[2]/div/div[2]/div[1]/div[2]/div[2]/form/div[5]/input")).click();
 		System.out.println("title of page:"+driver.getTitle());
-		
+		driver.findElement(By.xpath("/html/body/div[4]/div[1]/div[1]/div[2]/div[1]/ul/li[2]/a")).click();
+		driver.findElement(By.xpath("/html/body/div[4]/div[1]/div[1]/div[2]/div[1]/ul/li[2]/a")).click();
 	}
-	
+	@Test
+	public void login2() {
+		driver.findElement(By.xpath("/html/body/div[4]/div[1]/div[1]/div[2]/div[1]/ul/li[2]/a")).click();
+		driver.findElement(By.xpath("//*[@id=\"Email\"]")).sendKeys("www123@gmail.com");
+		driver.findElement(By.xpath("//*[@id=\"Password\"]")).sendKeys("wwwww123");
+		driver.findElement(By.xpath("//*[@id=\"RememberMe\"]")).click();
+		driver.findElement(By.xpath("/html/body/div[4]/div[1]/div[4]/div[2]/div/div[2]/div[1]/div[2]/div[2]/form/div[5]/input")).click();
+		System.out.println("title of page:"+driver.getTitle());
+		driver.findElement(By.xpath("/html/body/div[4]/div[1]/div[1]/div[2]/div[1]/ul/li[2]/a")).click();
+		driver.findElement(By.xpath("/html/body/div[4]/div[1]/div[1]/div[2]/div[1]/ul/li[2]/a")).click();
+	}
+
 	/*@AfterMethod
 	public void end() {
 		driver.close();
